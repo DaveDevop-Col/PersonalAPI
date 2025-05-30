536a1e73c0431abe16681ca33ee22b187fdd596f
DaveDevop-Col <dsjs2307@gmail.com>
2025-05-30 23:56:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
 const CACHE_NAME = 'trivia-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/icon-192.png',
-  '/icon-512.png',
-  // agrega aquí más archivos estáticos si tienes
+  './',
+  './index.html',
+  './style.css',
+  './js/script.js',    // Ruta corregida: ahora apunta a /js/script.js
+  './manifest.json',
+  './icon-only.png',
+  './icon-foreground.png',
+  './sw.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
